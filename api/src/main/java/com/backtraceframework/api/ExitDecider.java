9d9b0e1ee898399dd5b24c65f8bf111ea1a056eb
Haochen <haochen_233333@163.com>
2017-05-08 23:52:11 +0800

    Using Maven

--- api/src/main/java/com/backtraceframework/api/ExitDecider.java
+++ api/src/main/java/com/backtraceframework/api/ExitDecider.java
@@ -6,12 +6,12 @@ import java.util.Collection;
  * Created by Haochen on 2017/4/10.
  * <br/>
  * <br/>
- * {@link ResultHandler#onResultFound(int, Storage, Collection)}被调用后，
+ * {@link ResultHandler#onResultFound(int, Object, Collection)}被调用后，
  * 紧接着会调用{@link ExitDecider#shouldExit(int)}，判断是否应该停止算法
  */
 public interface ExitDecider {
     /**
-     * 紧接在{@link ResultHandler#onResultFound(int, Storage, Collection)}之后被调用，判断是否应该停止算法<br/>
+     * 紧接在{@link ResultHandler#onResultFound(int, Object, Collection)}之后被调用，判断是否应该停止算法<br/>
      * @return 只需找到一种结果: <code>true</code><br/>
      * 需要找到所有结果: <code>false</code><br/>
      * 需要找到n种结果: 设置一个 <code>int count = 0</code> 用来给结果计数，<br/>
